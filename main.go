0ce014a8e45b755e2e069afe51668e7381939f3d
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-06-15 15:35:38 +0900

    first

---
+++ main.go
@@ -0,0 +1,104 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"io/ioutil"
+	"os/exec"
+	"strings"
+
+	"github.com/google/gxui"
+	"github.com/google/gxui/drivers/gl"
+	"github.com/google/gxui/themes/dark"
+)
+
+func Fonts() ([]string, error) {
+	b, err := exec.Command("fc-list").Output()
+	if err != nil {
+		return nil, err
+	}
+	r := bytes.NewReader(b)
+	sc := bufio.NewScanner(r)
+	sc.Split(bufio.ScanLines)
+
+	fonts := make(map[string]struct{})
+	for sc.Scan() {
+		t := sc.Text()
+		s := strings.Split(t, ": ")[0]
+		fonts[s] = struct{}{}
+	}
+	res := make([]string, 0, len(fonts))
+	for key, _ := range fonts {
+		res = append(res, key)
+	}
+	return res, nil
+}
+
+func CreateList(theme gxui.Theme) gxui.List {
+	adapter := gxui.CreateDefaultAdapter()
+	items, err := Fonts()
+	if err != nil {
+		panic(err)
+	}
+	adapter.SetItems(items)
+
+	list := theme.CreateList()
+	list.SetAdapter(adapter)
+	list.SetOrientation(gxui.Vertical)
+
+	return list
+}
+
+func CreateText(theme gxui.Theme) gxui.TextBox {
+	text := theme.CreateTextBox()
+	text.SetText(`あのイーハトーヴォの
+すきとおった風、
+夏でも底に冷たさをもつ青いそら、
+うつくしい森で飾られたモーリオ市、
+郊外のぎらぎらひかる草の波。
+祇辻飴葛蛸鯖鰯噌庖箸
+ABCDEFGHIJKLM
+abcdefghijklm
+1234567890`)
+
+	return text
+}
+
+func appMain(driver gxui.Driver) {
+	theme := dark.CreateTheme(driver)
+
+	window := theme.CreateWindow(1920, 1080, "Hi")
+
+	splitter := theme.CreateSplitterLayout()
+	splitter.SetOrientation(gxui.Horizontal)
+
+	list := CreateList(theme)
+	splitter.AddChild(list)
+
+	b, err := ioutil.ReadFile("/usr/share/fonts/TTF/Ricty-Regular.ttf")
+	if err != nil {
+		panic(err)
+	}
+	font, err := driver.CreateFont(b, 32)
+	if err != nil {
+		panic(err)
+	}
+	text := CreateText(theme)
+	text.SetFont(font)
+
+	splitter.AddChild(text)
+
+	layout := theme.CreateLinearLayout()
+	layout.AddChild(splitter)
+	window.AddChild(layout)
+
+	window.OnClose(driver.Terminate)
+}
+
+func main() {
+	gl.StartDriver(appMain)
+}
